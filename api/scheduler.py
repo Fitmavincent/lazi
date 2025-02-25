be38ab136707ba6433b656ecb04d1dedb3d65842
Vincent Liang <7020812+Fitmavincent@users.noreply.github.com>
2025-02-25 22:17:46 +1000

    Add woolies scheduler

--- api/scheduler.py
+++ api/scheduler.py
@@ -1,10 +1,20 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from services.special_crawler.coles_crawler import ColesCrawler
+from services.special_crawler.woolies_crawler import WooliesCrawler
 
 async def fetch_coles_data():
     crawler = ColesCrawler()
     raw_data = await crawler.crawl_coles_pipeline()
+    if raw_data:
+        transformed_data = crawler.transform_product_data(raw_data)
+        crawler.save_to_file(transformed_data)
+        return {"status": "success"}
+    return {"status": "failed"}
+
+async def fetch_woolies_data():
+    crawler = WooliesCrawler()
+    raw_data = await crawler.crawl_woolies_pipeline()
     if raw_data:
         transformed_data = crawler.transform_product_data(raw_data)
         crawler.save_to_file(transformed_data)
@@ -24,6 +34,18 @@ def setup_scheduler():
             timezone='Australia/Sydney'
         ),
         id='fetch_coles_data'
+    )
+
+    # Add Woolworths scheduler with same schedule
+    scheduler.add_job(
+        fetch_woolies_data,
+        CronTrigger(
+            day_of_week='wed',
+            hour=0,
+            minute=0,
+            timezone='Australia/Sydney'
+        ),
+        id='fetch_woolies_data'
     )
 
     return scheduler
